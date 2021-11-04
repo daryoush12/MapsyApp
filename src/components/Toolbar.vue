6474f74aaf5e68c2a3bd257e8f0545e71bf8cc26
daryoush12 <farsimadan.daryoush@gmail.com>
2021-11-05 00:10:14 +0200

    add id's on menu buttons

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -3,16 +3,16 @@
         <div class="md-toolbar-row">
             <div class="md-toolbar-section-start">
                 <span class="md-title">Mapsy App</span>
-                <md-button class="md-primary" @click="setDrawingState(true)">
+                <md-button class="md-primary" @click="setDrawingState(true)" id="button_add_location">
                     <md-icon>add_location</md-icon>
                     Add Place
                 </md-button>
-                <md-button class="md-primary" @click="setDrawingState(false)">
+                <md-button class="md-primary" @click="setDrawingState(false)" id="button_stop_adding">
                     <md-icon>clear</md-icon>
                     Clear
                 </md-button>
 
-                <md-button class="md-primary" @click="invokeMapResetEvent">
+                <md-button class="md-primary" @click="invokeMapResetEvent" id="button_zoomout">
                     <md-icon>zoom_out_map</md-icon>
                     Reset Map
                 </md-button>
