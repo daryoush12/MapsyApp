26efadf0d7a380959f9e047ab51b4f746ae4467f
daryoush12 <farsimadan.daryoush@gmail.com>
2021-11-05 00:33:09 +0200

    fix formatting issues

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-import 'vue-select/dist/vue-select.css';
-import VueFilterDateFormat from '@vuejs-community/vue-filter-date-format';
+import 'vue-select/dist/vue-select.css'
+import VueFilterDateFormat from '@vuejs-community/vue-filter-date-format'
 import vSelect from 'vue-select'
 import VueTimepicker from 'vue2-timepicker'
 import 'vue2-timepicker/dist/VueTimepicker.css'
@@ -12,20 +12,17 @@ import store from './store/'
 import VueOpenLayers from './plugins/VueOpenLayer'
 
 Vue.config.productionTip = false
-Vue.use(VueMaterial);
-Vue.use(VueOpenLayers);
-Vue.use(VueFilterDateFormat);
+Vue.use(VueMaterial)
+Vue.use(VueOpenLayers)
+Vue.use(VueFilterDateFormat)
 
-Vue.component('vue-timepicker',VueTimepicker);
+Vue.component('vue-timepicker', VueTimepicker)
 Vue.component('v-select', vSelect)
 
-
 new Vue({
-  Data(){
-    return {
-     
-   }
-},
-store,
-  render: h => h(App),
+    Data() {
+        return {}
+    },
+    store,
+    render: (h) => h(App),
 }).$mount('#app')
