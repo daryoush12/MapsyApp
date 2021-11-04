54cf0fa8aae9821ba1e3118c8dabaeaaf9da0718
daryoush12 <farsimadan.daryoush@gmail.com>
2021-11-05 00:38:13 +0200

    rg

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,61 @@
 <template>
- <div class="page-container md-layout-row">
-    <md-app>
+    <div class="page-container md-layout-row">
+        <md-app>
             <md-app-toolbar class="md-primary">
                 <Toolbar />
             </md-app-toolbar>
-      <md-app-drawer md-permanent="full">
-      <drawercontent/>
-      </md-app-drawer>
-      <md-app-content>
-        <Content/>
-      </md-app-content>
-    </md-app>
+            <md-app-drawer md-permanent="full">
+                <drawercontent />
+            </md-app-drawer>
+            <md-app-content>
+                <Content />
+            </md-app-content>
+        </md-app>
     </div>
 </template>
 
 <script>
-
 import Content from './views/content'
 import Toolbar from './components/Toolbar'
 import drawercontent from './views/drawercontent'
 
 export default {
-  name: 'App',
-  components: {
-    Content,
-    Toolbar,
-    drawercontent
-  },
-    data(){
-      return {
-        errored: false,
-        loading: true,
-        showAlert: false,
-        shouldAddPlace: false,
-        shouldDraw: false,
-        currentab: "",
-      }
+    name: 'App',
+    components: {
+        Content,
+        Toolbar,
+        drawercontent,
     },
- 
-
-  }
-
+    data() {
+        return {
+            errored: false,
+            loading: true,
+            showAlert: false,
+            shouldAddPlace: false,
+            shouldDraw: false,
+            currentab: '',
+        }
+    },
+}
 </script>
 
-<style lang = "sass">
+<style lang="sass">
 .md-app
     min-height: 100vh
     border: 1px solid rgba(#000, .12)
-  
+
 .md-drawer
     width: 230px
     max-width: calc(100vw - 125px)
 
 
-  
+
 .load
    min-height: 100px
     width: 100px
     background-color: green
 
 
-.search-input 
+.search-input
  border-radius: 100px;
-
-
-   
-
-
-
 </style>
